80416418134bf4732f62242949f22544c9f94614
Mehraj Hosen <meherajhosen786@gmail.com>
2025-05-30 23:42:44 +0600

    benifits add

--- src/components/benifits/Benifits.jsx
+++ src/components/benifits/Benifits.jsx
@@ -8,39 +8,34 @@ const Reviews = () => {
   const reviews=[
     {
       img: img1,
-      name: 'Chloe Anderson',
-      star: "star",
-      review: "“Very happy with their work! The team was efficient and the new system runs great.”"
+      title: '24/7 Emergency Services',
+      content: "Highly trained and certified HVAC experts with years of industry experience."
     },
     {
       img: img1,
-      name: 'Chloe Anderson',
-      star: "star",
-      review: "“Very happy with their work! The team was efficient and the new system runs great.”"
+      title: '24/7 Emergency Services',
+      content: "Highly trained and certified HVAC experts with years of industry experience."
     },
     {
       img: img1,
-      name: 'Chloe Anderson',
-      star: "star",
-      review: "“Very happy with their work! The team was efficient and the new system runs great.”"
+      title: '24/7 Emergency Services',
+      content: "Highly trained and certified HVAC experts with years of industry experience."
     },
   ]
   return (
-    <div id='reviews' className='pt-[80px] pb-[50px]'>
+    <div  className='py-[50px] bg-secondary'>
       <Container>
-        <h2 className='text-center mb-10 font-bold text-[40px]'>Trusted by Families Like Yours</h2>
+        <h2 className='text-center mb-10 font-bold text-[40px]'>The Benefits of Choosing Us</h2>
 
         <Flex>
           {
             reviews.map((review)=>(
-              <div className='py-[23px] rounded-[50px] w-[360px] shadow-2xl group hover:bg-primary text-center px-[58px] text-black'>
+              <div className='py-[32px] rounded-[20px] w-[360px] bg-white group hover:bg-primary text-center px-[25px] text-black'>
                 <Flex className="flex-col group-hover:text-white justify-center">
                   <img src={review.img} alt="" />
-                  <h3 className='mt-2'>{review.name}</h3>
-                  <span>{review.star}</span>
-                  <p className='py-5'>{review.review}</p>
-                  <span>goIcon</span>
-                  <p>Reviewed on Google</p>
+                  <h3 className='mt-3 text-heading3 font-medium leading-[32px] mb-1'>{review.title}</h3>
+                  <p className='py-5 text-[#666666]'>{review.content}</p>
+                  
 
 
 
@@ -51,9 +46,6 @@ const Reviews = () => {
             ))
           }
         </Flex>
-        <div className='text-center'>
-        <Button className="mt-8 !py-[12px] !px-[24px] text-center">View More</Button>
-        </div>
 
       </Container>
     </div>
